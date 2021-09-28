f2fb109afb44ea33cb63cd2f3a5fa9386b57372b
Muhammad Eryawan <eryawanir@gmail.com>
2021-09-28 11:20:28 +0700

    membuat template admin

---
+++ app/Views/layouts_admin/template_admin.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title; ?></title>
+  <link rel="icon" href="/assets/favicon.ico">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/fontawesome-free/css/all.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/daterangepicker/daterangepicker.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/toastr/toastr.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <!-- Tempusdominus Bbootsrap 4 -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootsrap-4.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="/assets/adminlte3/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/assets/adminlte3/dist/css/adminlte3.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <!-- Site wrapper -->
+  <div class="wrapper">
+    <!-- Navbar -->
+    <?= $this->include('layouts_admin/navbar'); ?>
+    <!-- .navbar -->
+
+    <!-- Main Sidebar Container -->
+    <?= $this->include('layouts_admin/sidebar'); ?>
+
+    <!-- Content Wrapper. Contain page content -->
+    <?= $this->renderSection('content'); ?>
+    <!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0.0
+      </div>
+      <strong>Copyright
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+        <a href="#">University</a>.
+      </strong> All rights reserverd.
+    </footer>
+  </div>
+
+  <!-- JQuery -->
+  <script src="/assets/adminlte3/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="/assets/adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- InputMask -->
+  <script src="/assets/adminlte3/plugins/moment/moment.min.js"></script>
+  <script src="/assets/adminlte3/plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+  <!-- date range picker -->
+  <script src="/assets/adminlte3/plugins/daterangepicker/datetrangepicker.js"></script>
+  <!-- SweetAlert3 -->
+  <script src="/assets/adminlte3/plugins/sweetalert2/sweetalert2.min.js"></script>
+  <!-- Toastr -->
+  <script src="/assets/adminlte3/plugins/toastr/toastr.min.js"></script>
+  <!-- Datatables -->
+  <script src="/assets/adminlte3/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="/assets/adminlte3/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="/assets/adminlte3/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="/assets/adminlte3/plugins/datatables-responsive/js/responsive.bootsrap4.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="/assets/adminlte3/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <!-- Tempusdominis Bootstrap 4 -->
+  <script src="/assets/adminlte3/plugins/tempusdominus-bootsrap-4/js/tempusdominus-bootsrap-4.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="/assets/adminlte3/dist/js/adminlte.min.js"></script>
+  <!-- page script -->
+  <?= $this->renderSection('script'); ?>
+
+</body>
+
+</html>
